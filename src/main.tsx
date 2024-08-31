a1b904c4ee53cfb16f650908c99fe2e7a0a243f7
Olshanskaya <viktoriarubenovna@gmail.com>
2024-08-31 14:15:39 +0300

    tanstack devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import App from "./App";
 import "./index.css";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </BrowserRouter>
   </QueryClientProvider>
 );
